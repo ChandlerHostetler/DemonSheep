7bf4f341b9273f593e013cc2a71bd94055c8cf87
Chandler <chosteler@mail.pima.edu>
2019-05-26 05:07:21 -0700

    Added movement and shooting

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public GameObject player;
+    public int speed;
+    public List<BoxCollider> walls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (BoxCollider wall in walls)
+        {
+            float xRange = UnityEngine.Random.Range(-wall.bounds.extents.x, wall.bounds.extents.x);
+            float yRange = UnityEngine.Random.Range(-wall.bounds.extents.y, wall.bounds.extents.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        foreach (BoxCollider wall in walls)
+        {
+            if (wall.bounds.Contains(player.transform.position))
+            {
+                player.transform.position = wall.bounds.ClosestPoint(player.transform.position);
+            }
+        }
+    }
+
+    public void move()
+    {
+        float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        player.transform.Translate(x, y, 0f);
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject bullet;
+    public int speed;
+    public float coolDownTime;
+    private float nextFiretime;
+    public List<GameObject> bullets;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+
+        fire();
+    }
+
+    public void shoot()
+    {
+
+      
+        Vector2 playerPos = player.transform.position;
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenPos = Camera.main.ScreenToWorldPoint(new Vector2(mousePos.x, mousePos.y));
+        Quaternion q = Quaternion.FromToRotation(Vector2.up, screenPos - playerPos);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (transform.localScale.x < 0 && nextFiretime < Time.time)
+            {
+                nextFiretime = Time.time + coolDownTime;
+                bullet = Instantiate(bullet, new Vector2(playerPos.x, playerPos.y), q);
+                bullets.Add(bullet);
+
+            }
+            else if (nextFiretime < Time.time)
+            {
+                nextFiretime = Time.time + coolDownTime;
+                bullet = Instantiate(bullet, new Vector2(playerPos.x, playerPos.y), q);
+                bullets.Add(bullet);
+     
+            }
+
+        }
+     
+   }
+
+    public void fire()
+    {
+        //bullet.GetComponent<Rigidbody2D>().AddForce(bullet.GetComponent<Rigidbody2D>().transform.up * speed);
+        foreach (GameObject bullet in bullets)
+        {
+            bullet.transform.Translate(Vector2.up * Time.deltaTime * speed); //this works?!?!?!?!?!?
+        }
+    }
+
+}
